47e5b2fb019a0facaa40dba3c1d95121fc89bf3a
sreInk <srinknok@gmail.com>
2024-10-09 09:06:29 +0530

    lllllll

---
+++ Current date and time.py
@@ -0,0 +1,6 @@
+from datetime import date , time , datetime
+today = date.today()
+now = datetime.now()
+print("Today date is",today)
+print("The Time is is" ,now)
+print("Total is" ,today.year,"-",today.month,"-",today.day)
